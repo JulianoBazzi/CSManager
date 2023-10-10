958bdb0ddc659ef64110451790008a1fc358930e
Juliano Bazzi <julibazzi@hotmail.com>
2023-10-10 12:16:03 -0400

    * Atualização para o CS2

--- src/assets/games.ts
+++ src/assets/games.ts
@@ -2,14 +2,9 @@ import ISelectOption from '~/models/ISelectOption';
 
 export const games: ISelectOption[] = [
   {
-    id: 'cs',
-    name: 'Counter-Strike 1.6',
-    shortName: 'CS 1.6',
-  },
-  {
-    id: 'cscz',
-    name: 'Counter-Strike: Condition Zero',
-    shortName: 'CS:CZ',
+    id: 'cs2',
+    name: 'Counter-Strike 2',
+    shortName: 'CS2',
   },
   {
     id: 'css',
@@ -17,8 +12,13 @@ export const games: ISelectOption[] = [
     shortName: 'CS:S',
   },
   {
-    id: 'csgo',
-    name: 'Counter-Strike: Global Offensive',
-    shortName: 'CS:GO',
+    id: 'cscz',
+    name: 'Counter-Strike: Condition Zero',
+    shortName: 'CS:CZ',
+  },
+  {
+    id: 'cs',
+    name: 'Counter-Strike 1.6',
+    shortName: 'CS 1.6',
   },
 ];
