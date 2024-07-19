c3beea798c77bcfb3c333b19be54a1f1c7eccd63
Juliano Bazzi <julibazzi@hotmail.com>
2024-07-19 17:40:07 -0400

    * Sorteio dos times com base no ranking dos mapas 4

--- src/utils/balanceTeams.ts
+++ src/utils/balanceTeams.ts
@@ -7,12 +7,15 @@ function balanceTeams(players: IPlayerScoreAPI[]): [IPlayerScoreAPI[], IPlayerSc
     return Math.abs(score1 - score2);
   }
 
+  function calculateTeamScore(team: IPlayerScoreAPI[]): number {
+    return team.reduce((sum, player) => sum + player.score, 0);
+  }
+
   function generateTeams(playerList: IPlayerScoreAPI[]): [IPlayerScoreAPI[], IPlayerScoreAPI[]] {
     const playersByStart: { [key: number]: IPlayerScoreAPI[] } = {};
     for (let i = 0; i <= 5; i += 1) {
       playersByStart[i] = [];
     }
-
     playerList.forEach((player) => {
       playersByStart[player.star].push(player);
     });
@@ -30,13 +33,52 @@ function balanceTeams(players: IPlayerScoreAPI[]): [IPlayerScoreAPI[], IPlayerSc
       });
     }
 
+    while (Math.abs(team1.length - team2.length) > 1) {
+      const sourceTeam = team1.length > team2.length ? team1 : team2;
+      const targetTeam = team1.length < team2.length ? team1 : team2;
+
+      const worstPlayerIndex = sourceTeam.reduce((worstIdx, player, idx, arr) => {
+        const worstPlayer = arr[worstIdx];
+        if (player.star < worstPlayer.star || (player.star === worstPlayer.star && player.score < worstPlayer.score)) {
+          return idx;
+        }
+        return worstIdx;
+      }, 0);
+
+      const playerToMove = sourceTeam.splice(worstPlayerIndex, 1)[0];
+      if (playerToMove) {
+        targetTeam.push(playerToMove);
+      }
+    }
+
+    if (team1.length !== team2.length) {
+      const score1 = calculateTeamScore(team1);
+      const score2 = calculateTeamScore(team2);
+
+      const sourceTeam = score1 > score2 ? team1 : team2;
+      const targetTeam = score1 > score2 ? team2 : team1;
+
+      const worstPlayerIndex = sourceTeam.reduce((worstIdx, player, idx, arr) => {
+        const worstPlayer = arr[worstIdx];
+        if (player.star < worstPlayer.star || (player.star === worstPlayer.star && player.score < worstPlayer.score)) {
+          return idx;
+        }
+        return worstIdx;
+      }, 0);
+
+      const playerToMove = sourceTeam.splice(worstPlayerIndex, 1)[0];
+      if (playerToMove) {
+        targetTeam.push(playerToMove);
+      }
+    }
+
     return [team1, team2];
   }
 
   let bestTeams: [IPlayerScoreAPI[], IPlayerScoreAPI[]] = [[], []];
   let smallestDifference = Infinity;
 
-  for (let i = 0; i < 100; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     const [team1, team2] = generateTeams(players);
     const difference = calculateScoreDifference(team1, team2);
 
